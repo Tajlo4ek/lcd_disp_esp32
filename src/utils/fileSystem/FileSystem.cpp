950487c583f123380d5207e7a13b4b07f847e01e
Tajlok <aleksey.belyaev.asd@gmail.com>
2022-01-27 21:26:32 +0300

    add mutex to filesystem

--- src/utils/fileSystem/FileSystem.cpp
+++ src/utils/fileSystem/FileSystem.cpp
@@ -1,69 +1,79 @@
 #include "FileSystem.h"
 
 #include "utils/FileNames.h"
+#include "utils/MutexUtils.h"
 
 #define CONFIG_LITTLEFS_SPIFFS_COMPAT 1
 
 namespace FileSystem
 {
-    bool isInit = false;
+    SemaphoreHandle_t fsMutex = xSemaphoreCreateMutex();
 
     void Init()
     {
+        fsMutex = xSemaphoreCreateMutex();
+
         LITTLEFS.begin();
 
         LITTLEFS.mkdir(CONFIG_DIR);
         LITTLEFS.mkdir(PAGES_DIR);
-
-        isInit = true;
-    }
-
-    inline void CheckInit()
-    {
-        if (isInit == false)
-        {
-            Init();
-        }
     }
 
     String ReadFile(const String &fileName)
     {
-        CheckInit();
+        String data;
 
-        if (LITTLEFS.exists(fileName) == true)
-        {
-            File file = LITTLEFS.open(fileName, "r");
-            auto data = file.readString();
-            file.close();
-            return data;
-        }
+        MutexUtils::MutexTask(fsMutex, [&fileName, &data]() mutable
+                              {
+                                  if (LITTLEFS.exists(fileName) == true)
+                                  {
+                                      File file = LITTLEFS.open(fileName, "r");
+                                      data = file.readString();
+                                      file.close();
+                                  }
+                              });
 
-        return String();
+        return data;
     }
 
     void WriteFile(const String &fileName, const String &data)
     {
-        File file = LITTLEFS.open(fileName, FILE_WRITE);
-        file.print(data.c_str());
-        file.close();
+        MutexUtils::MutexTask(fsMutex, [&fileName, &data]() mutable
+                              {
+                                  File file = LITTLEFS.open(fileName, FILE_WRITE);
+                                  file.print(data.c_str());
+                                  file.close();
+                              });
     }
 
     const bool FileExists(const String &fileName)
     {
-        CheckInit();
-        return LITTLEFS.exists(fileName);
+        bool exist;
+
+        MutexUtils::MutexTask(fsMutex, [&fileName, &exist]() mutable
+                              { exist = LITTLEFS.exists(fileName); });
+
+        return exist;
     }
 
     File OpenFile(const String &path, const char *mode)
     {
-        CheckInit();
-        return LITTLEFS.open(path, mode);
+        File file;
+
+        MutexUtils::MutexTask(fsMutex, [&path, &mode, &file]() mutable
+                              { file = LITTLEFS.open(path, mode); });
+
+        return file;
     }
 
     const bool DeleteFile(const String &path)
     {
-        CheckInit();
-        return LITTLEFS.remove(path);
+        bool deleted;
+
+        MutexUtils::MutexTask(fsMutex, [&path, &deleted]() mutable
+                              { deleted = LITTLEFS.remove(path); });
+
+        return deleted;
     }
 
 }
